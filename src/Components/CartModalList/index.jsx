c69b02690ec62ccbe364a85e231bd4e3efaf1fd2
Henrique <you@example.com>
2023-06-14 11:40:49 -0300

    funcionalidade 4. Fechar o carrinho com esc, clicando fora do modal e clicando no botão de fechar pronta

--- src/Components/CartModalList/index.jsx
+++ src/Components/CartModalList/index.jsx
@@ -4,24 +4,39 @@ import { CartModalCard } from "../CartModalCard"
 
 export const CartModalList = ( {setIsOpen} ) => {
     const modalRef = useRef(null)
-    
     useEffect(() => {
-        const handleOutClick = () => {
-            console.log('clicou')
+        const handleOutClick = (event) => {
+            if(!modalRef.current?.contains(event.target)){
+                setIsOpen(false)
+            }
         }
 
         window.addEventListener('mousedown', handleOutClick)
         
         return () => {
             window.removeEventListener('mousedown', handleOutClick)
-
         }
     }, [])
 
+    const buttonRef = useRef(null)
+    useEffect(() => {
+        const handleKeyDown = (event) => {
+            if(event.key === 'Escape'){
+                setIsOpen(false)
+            }
+        }
+
+        window.addEventListener('keydown', handleKeyDown)
+
+        return () => (
+            window.removeEventListener('keydown', handleKeyDown)
+        )
+    }, [])
+
     return(
-        <div>
+        <div ref={modalRef}>
             <h1>oi</h1>
-            <button onClick={() => setIsOpen(false)}>Fechar</button>
+            <button ref={buttonRef} onClick={() => setIsOpen(false)}>Fechar</button>
         </div>
         // <ul>
         //     {
